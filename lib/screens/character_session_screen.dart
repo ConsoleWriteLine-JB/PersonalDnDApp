5e78ec2e0ec5ded48d54dac3f7111f61dce8c0e9
James <balljames05@gmail.com>
2026-04-01 20:32:47 +0200

    Added Saving Throws

--- lib/screens/character_session_screen.dart
+++ lib/screens/character_session_screen.dart
@@ -42,7 +42,7 @@ class _CharacterSessionScreenState extends State<CharacterSessionScreen> {
                         ),
                         const SizedBox(height: 8),
                         Text(
-                          "SUBJECT: IRON-GEAR GNOMISH DELVER //\nREGISTRY: 89-XJ",
+                          "SUBJECT: ARTIFICER GNOME //\nREGISTRY: QUIVER //",
                           style: TextStyle(
                             color: Color(0xFFACCFBA),
                             fontSize: 10,
@@ -85,6 +85,8 @@ class _CharacterSessionScreenState extends State<CharacterSessionScreen> {
                       )
                     ),
 
+                    SizedBox(width: 5,),
+
                     Expanded(
                       child: _darkCard(
                         "Movement Speed",
@@ -105,40 +107,284 @@ class _CharacterSessionScreenState extends State<CharacterSessionScreen> {
                   child: Padding(
                     padding: const EdgeInsets.only(left: 20, right: 20),
                     child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(height: 25,),
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(height: 25,),
 
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "STRUCTURAL INTEGRITY [HP]",
-                            style: TextStyle(
-                              color: Colors.grey, 
-                              fontSize: 10, 
-                              fontWeight: FontWeight.bold, 
-                              letterSpacing: 1.0
-                            )
-                          ),
-                          RichText(
-                            text: const TextSpan(
-                              children: [
-                                TextSpan(text: "84", style: TextStyle(color: Color(0xFFFFB77C), fontSize: 24, fontWeight: FontWeight.bold)),
-                                TextSpan(text: " / 84", style: TextStyle(color: Colors.grey, fontSize: 16)),
-                              ]
-                            )
-                          ),
-                        ],
-                      ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "STRUCTURAL INTEGRITY [HP]",
+                              style: TextStyle(
+                                color: Colors.grey, 
+                                fontSize: 10, 
+                                fontWeight: FontWeight.bold, 
+                                letterSpacing: 1.0
+                              )
+                            ),
+                            RichText(
+                              text: const TextSpan(
+                                children: [
+                                  TextSpan(text: "84", style: TextStyle(color: Color(0xFFFFB77C), fontSize: 24, fontWeight: FontWeight.bold)),
+                                  TextSpan(text: " / 84", style: TextStyle(color: Colors.grey, fontSize: 16)),
+                                ]
+                              )
+                            ),
+                          ],
+                        ),
 
-                      const SizedBox(height: 12),
-                      // A quick placeholder for your HP Bar
-                      Container(height: 12, color: const Color(0xFFFFB77C)),
-                    ]
+                        const SizedBox(height: 12),
+                        // A quick placeholder for your HP Bar
+                        Container(height: 12, color: const Color(0xFFFFB77C)),
+                      ]
+                    )
                   )
+                ),
+
+                SizedBox(height: 16),
+
+                Container(
+                  decoration: BoxDecoration(
+                    color: Color(0XFF0E0E0E),
+                    border: Border.all(
+                      color: Color.fromARGB(255, 24, 24, 24)
+                    )
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Hardened Circuits (Saves)",
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Color(0XffACCFBA),
+                          )
+                        ),
+
+                        SizedBox(height: 10,),
+                        
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Expanded(
+                              child: Container(
+                                height: 60,
+                                decoration: BoxDecoration(
+                                  color: Color.fromARGB(117, 255, 183, 124),
+                                  border: Border.all(
+                                    color: Color.fromARGB(255, 24, 24, 24)
+                                  )
+                                ),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    SizedBox(height: 10),
+
+                                    Text(
+                                      "STR",
+                                      style: TextStyle(
+                                        color: Color(0XFFFFB77C),
+                                      ),
+                                    ),
+                                    
+                                    Text(
+                                      "+5",
+                                      style: TextStyle(
+                                        color: Color(0XFFFFB77C),
+                                      ),
+                                    )
+                                  ]
+                                )
+                              ),
+                            ),
+
+                            SizedBox(width: 10,),
+
+                            Expanded(
+                              child: Container(
+                                height: 60,
+                                decoration: BoxDecoration(
+                                  color: Colors.transparent,
+                                  border: Border.all(
+                                    color: Color.fromARGB(255, 24, 24, 24)
+                                  )
+                                ),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    SizedBox(height: 10),
+
+                                    Text(
+                                      "DEX",
+                                      style: TextStyle(
+                                        color: Colors.grey,
+                                      )
+                                    ),
+                                    
+                                    Text(
+                                      "+1",
+                                      style: TextStyle(
+                                        color: Colors.grey,
+                                      )
+                                    )
+                                  ]
+                                )
+                              ),
+                            ),
+
+                            SizedBox(width: 10,),
+                            
+                            Expanded(
+                              child: Container(
+                                height: 60,
+                                decoration: BoxDecoration(
+                                  color: Colors.transparent,
+                                  border: Border.all(
+                                    color: Color.fromARGB(255, 24, 24, 24)
+                                  )
+                                ),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    SizedBox(height: 10),
+
+                                    Text(
+                                      "CON",
+                                      style: TextStyle(
+                                        color: Colors.grey,
+                                      )
+                                    ),
+                                    
+                                    Text(
+                                      "+3",
+                                      style: TextStyle(
+                                        color: Colors.grey,
+                                      )
+                                    )
+                                  ]
+                                )
+                              ),
+                            ),
+                            
+                          ]
+                        ),
+
+                        SizedBox(height: 10,),
+
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Expanded(
+                              child:  Container(
+                                height: 60,
+                                decoration: BoxDecoration(
+                                  color: Colors.transparent,
+                                  border: Border.all(
+                                    color: Color.fromARGB(255, 24, 24, 24)
+                                  )
+                                ),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    SizedBox(height: 10),
+
+                                    Text(
+                                      "INT",
+                                      style: TextStyle(
+                                        color: Colors.grey,
+                                      )
+                                    ),
+                                    
+                                    Text(
+                                      "+5",
+                                      style: TextStyle(
+                                        color: Colors.grey,
+                                      )
+                                    )
+                                  ]
+                                )
+                              ),
+                            ),
+
+                            SizedBox(width: 10,),
+
+                            Expanded(
+                              child:  Container(
+                                height: 60,
+                                decoration: BoxDecoration(
+                                  color: Colors.transparent,
+                                  border: Border.all(
+                                    color: Color.fromARGB(255, 24, 24, 24)
+                                  )
+                                ),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    SizedBox(height: 10),
+
+                                    Text(
+                                      "WIS",
+                                      style: TextStyle(
+                                        color: Colors.grey,
+                                      )
+                                    ),
+                                    
+                                    Text(
+                                      "+0",
+                                      style: TextStyle(
+                                        color: Colors.grey,
+                                      )
+                                    )
+                                  ]
+                                )
+                              ), 
+                            ),
+
+                            SizedBox(width: 10,),
+                           
+                            Expanded(
+                              child: Container(
+                                height: 60,
+                                decoration: BoxDecoration(
+                                  color: Colors.transparent,
+                                  border: Border.all(
+                                    color: Color.fromARGB(255, 24, 24, 24)
+                                  )
+                                ),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    SizedBox(height: 10),
+
+                                    Text(
+                                      "CHA",
+                                      style: TextStyle(
+                                        color: Colors.grey,
+                                      )
+                                    ),
+                                    
+                                    Text(
+                                      "-2",
+                                      style: TextStyle(
+                                        color: Colors.grey,
+                                      )
+                                    )
+                                  ]
+                                )
+                              ),
+                            ),
+                          ]
+                        ),
+                      ]
+                    )
                   )
-                )
+                ),
+
+                //Hierso
+
               ],
             )
           )
